d45963f0adc2b025d4876a54c405686ecce64946
Nuthan B <NuthanB@protonmail.com>
2024-04-04 19:00:50 +0530

    Login button

--- fixmart-admin/app/page.js
+++ fixmart-admin/app/page.js
@@ -3,8 +3,10 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-    <div className="bg-red-200">
-      Hi
+    <div className="bg-blue-900 w-screen h-screen flex items-center">
+      <div className="text-center w-full">
+      <button className="bg-white p-2 px-4 rounded-lg">Login with Google</button>
+      </div>
     </div>
     </main>
   );
