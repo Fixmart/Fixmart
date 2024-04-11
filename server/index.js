bda03b143f05160d17f7964f16d06857da4e8bdc
Nuthan B <NuthanB@protonmail.com>
2024-04-11 16:41:10 +0530

    Added the required modals

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db.js');
+const Customer = require('./modals/customer.js');
+const Product  = require('./modals/products.js');
+const Order  = require('./modals/orders.js');
+const Collection  = require('./modals/collections.js');
+
 connectDB();
 const app = express();
-const port = 3000; // or any port you prefer
+const port = 3001; // or any port you prefer
 
 // Middleware to parse JSON bodies
 app.use(express.json());
---
+++ server/modals/customer.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const customerSchema = new mongoose.Schema({
+  clerkId: String,
+  name: String,
+  email: String,
+  orders: {
+    type: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order"}]
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+const Customer = mongoose.models.Customer || mongoose.model("Customer", customerSchema);
+
+module.exports = Customer;
