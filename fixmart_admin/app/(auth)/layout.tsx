9e8366abe9a2e3e34efd3a599685fadb566512e4
Nuthan B <NuthanB@protonmail.com>
2025-04-29 11:52:25 +0530

    Updated the code

--- fixmart_admin/app/(auth)/layout.tsx
+++ fixmart_admin/app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Saasiple - Admin Auth",
+  title: "SaasipleEcommerce - Admin Auth",
   description: "Admin Auth",
 };
 
--- fixmart_admin/app/(dashboard)/layout.tsx
+++ fixmart_admin/app/(dashboard)/layout.tsx
@@ -10,7 +10,7 @@ import { ToasterProvider } from "@/lib/ToasterProvider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Saasiple - Admin Dashboard",
+  title: "SaasipleEcommerce - Admin Dashboard",
   description: "Admin dashboard to manage Saasiple",
 };
 
